24f50c8260da34dddd047846285fd24aef6d7553
muzzammil1704-droid <muzzammil1704@gmail.com>
2026-09-23 11:57:24 +0530

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase project: Ramnad Gedgets
+const firebaseConfig = {
+  apiKey: "AIzaSyBtS7GE-DB3mrCgiUXAZzdqN7U7LbS1HO4",
+  authDomain: "ramnad-gedgets-4406c.firebaseapp.com",
+  projectId: "ramnad-gedgets-4406c",
+  storageBucket: "ramnad-gedgets-4406c.firebasestorage.app",
+  messagingSenderId: "86317866392",
+  appId: "1:86317866392:web:d0ec376bfd3211549a9fed",
+  measurementId: "G-210RQF0NYE"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
